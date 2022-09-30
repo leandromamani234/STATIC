08f818442ba7ef94f722099cf19b78da76696dc9
leandromamani234 <le1059583@gmail.com>
2022-09-30 09:54:30 -0400

    static

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Util.DOLARES);
+        double pesos;
+        pesos=Util.bolivianosPesos(1000);
+        System.out.println(pesos);
+    }
+
+}
---
+++ src/Util.java
@@ -0,0 +1,9 @@
+public class Util {
+    public static final double DOLARES = 6.96;
+    public static final double EUROS = 6.73;                 //constantes estaticas
+    public static final double ARGETINO = 21.33;
+    public static double  bolivianosPesos(double bolivianos){
+        return bolivianos * ARGETINO;
+    }
+
+}
